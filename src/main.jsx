9e81b99c0a3d9be1fe6a73c5a60bd9a618bb59ff
devppratik <pratikpanda24@gmail.com>
2021-10-10 22:52:21 +0530

    Added New Folder Structure for the Project

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import IndexRouter from "./routes";
+
+const client = new QueryClient();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  <QueryClientProvider client={client}>
+    <IndexRouter />
+  </QueryClientProvider>,
+  document.getElementById("root")
+);
