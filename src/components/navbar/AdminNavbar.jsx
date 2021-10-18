83ccb61fe0258bd353c237b971bcdf66735b3c7b
prototype-raj <rjrshi1rakshit@gmail.com>
2021-10-18 12:19:59 +0530

    Fixed the navbar issue

--- src/components/navbar/AdminNavbar.jsx
+++ src/components/navbar/AdminNavbar.jsx
@@ -75,7 +75,7 @@ const Header = (props) => {
                   <Drawer sections={["1", "2"]} />
                 ) : (
                 <Link to="#" className={classes.subscribeButton}>
-                    BUTTON 1
+                    PROFILE
                   </Link>
                 )}
                 <Box>
